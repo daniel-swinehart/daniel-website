f61c824250886e0910d8a172e5ef94510c021074
daniel-swinehart <d4570s@gmail.com>
2021-10-18 12:26:55 +0100

    first commit

---
+++ js/maroc.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById('top-menu-drop').style.width = "150px";
+};
+
+function closeNav() {
+  document.getElementById("top-menu-drop").style.width = "0";
+}
