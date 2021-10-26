91502868b74dee802f8461b3d5d06d41b34f5f2b
daniel-swinehart <d4570s@gmail.com>
2021-10-26 11:10:54 +0100

    email form verification

--- email-submit.php
+++ email-submit.php
@@ -1,5 +1,15 @@
 <?php 
-if(isset($_POST['submit'])){
+ob_start();
+
+//Check for the required fields from 'email-form' on contact.html
+if ((!$_POST['email-first-name']) || (!$_POST['email-last-name']) || (!$_POST['email-address']) || (!$_POST['email-message'])) {
+   header("Location: contact.html");
+   exit;
+}
+if (!filter_var(($_POST['email-message']), FILTER_VALIDATE_EMAIL)) {
+    header("Location: contact.html");
+    exit;
+} else {
     $to = "daniel@danielmaroc.com"; // this is your Email address
     $from = $_POST['email-address']; // this is the sender's Email address
     $first_name = $_POST['email-first-name'];
@@ -15,5 +25,8 @@ if(isset($_POST['submit'])){
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     
     header('Location: contact-thank-you.html');
-    }
+
+
+}
+ob_flush();
 ?>
